2d2be8d0c29d8bfaaef1c7692c3ea65a1a3b7fe3
Veronika Ulyanchik <veronikaullll1998@gmail.com>
2022-02-23 18:45:50 +0300

    HW8

--- homework/src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ homework/src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,13 +1,52 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+import {UserType} from "../HW8";
+
+type PayloadType = 'up' | 'down';
+
+export type SortActionType = {
+    type: 'sort'
+    payload: PayloadType
+}
+export type CheckActionType = {
+    type: 'check'
+    payload: number
+}
+
+type ActionType = SortActionType | CheckActionType
+
+export const homeWorkReducer = (state: Array<UserType>, action: ActionType): Array<UserType> => { // need to fix any
     switch (action.type) {
         case 'sort': {
-            // need to fix
-            return state
+            switch (action.payload) {
+                case "down":
+                    let copyState = [...state]
+                    return copyState.sort((a, b) => a.name > b.name ? -1 : 1)
+                case "up":
+                    let copyState1 = [...state]
+                    return copyState1.sort((a, b) => a.name > b.name ? 1 : -1)
+                default:
+                    return state
+            }
         }
         case 'check': {
             // need to fix
-            return state
+            return state.filter(a => a.age > action.payload)
         }
-        default: return state
+        default:
+            return state
+    }
+}
+
+
+export const SortAC = (value: PayloadType): SortActionType => {
+    return {
+        type: 'sort',
+        payload: value
+    }
+}
+
+export const checkAC = (age: number): CheckActionType => {
+    return {
+        type: 'check',
+        payload: age
     }
 }
